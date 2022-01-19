8baf6cf80df816f92afc656cb5465c59c902eee3
waris8 <gwaris98@gmail.com>
2022-01-19 23:30:16 +0530

    isPalindrome done

---
+++ leetcode/src/IsPalindrome.cpp
@@ -0,0 +1,28 @@
+/*
+ * IsPalindrome.cpp
+ *
+ *  Created on: 19-Jan-2022
+ *      Author: waris
+ */
+
+#include "IsPalindrome.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class IsPalindrome{
+public:
+	bool isPalindrome(int x){
+		string a = to_string(x);
+		int temp = 0;
+		while(temp < a.size()/2){
+			if(a[temp] == a[a.size()-1-temp]){
+				temp++;
+			}
+			else{
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ leetcode/src/IsPalindrome.h
@@ -0,0 +1,16 @@
+/*
+ * IsPalindrome.h
+ *
+ *  Created on: 19-Jan-2022
+ *      Author: waris
+ */
+
+#ifndef ISPALINDROME_H_
+#define ISPALINDROME_H_
+
+class IsPalindrome {
+public:
+	bool isPlaindrome(int x);
+};
+
+#endif /* ISPALINDROME_H_ */
